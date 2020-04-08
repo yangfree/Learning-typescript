ec11f59827d653370462b286bfbcbe7554ba0c5f
jie <414871205@qq.com>
2020-04-08 18:47:47 +0800

    完毕

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("新用户链接了");
+  // 用户登陆
   socket.on("login", (data) => {
     //   console.log(data);
     let user = users.find((item) => data.username === item.username);
@@ -30,6 +31,32 @@ io.on("connection", (socket) => {
       io.emit("addUser", data);
       // 广播给所有用户当前聊天室用户数
       io.emit("userList", users);
+      // 存储用户信息 登出等时候会更新状态
+      socket.username = data.username;
+      socket.avatar = data.avatar;
     }
+  });
+  // 用户断开链接
+  socket.on("disconnect", () => {
+    let id = users.findIndex((item) => {
+      item.username === socket.username;
+    });
+    users.splice(id, 1);
+    io.emit("delUser", {
+      username: socket.username,
+      avatar: socket.avatar,
+    });
+    io.emit("userList", users);
+  });
+
+  //监听聊天消息
+  socket.on("sendMsg", (data) => {
+    // console.log(data);
+    io.emit("receiveMsg", data);
+  });
+  //接受图片信息
+  socket.on("sendImage", (data) => {
+    // console.log(data);
+    io.emit("receiveImage", data);
   });
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 // 链接io服务
 const socket = io("ws://localhost:3002");
+let username, avatar;
 
 // 登陆功能
 $("#loginPhotos li").on("click", function () {
@@ -15,7 +16,6 @@ $("#loginBtn").on("click", function () {
   }
   // 获取用户头像
   let avatar = $("#loginPhotos li.actived img").attr("src");
-  console.log(username, avatar);
   // 链接io服务器
   socket.emit("login", {
     username: username,
@@ -33,16 +33,145 @@ socket.on("loginSuccess", (data) => {
   // console.log(data);
   $("#chatList .my-photo").attr("src", data.avatar);
   $("#chatList .my-username").text(data.username);
+  username = data.username;
+  avatar = data.avatar;
 });
 
 // 监听添加新用户
 socket.on("addUser", (data) => {
-  $("#users").html(`
+  $("#users").append(`
     <div class="broadcast">${data.username}加入了群聊</div>
     `);
+  scrollIntoView();
 });
 
 // 监听聊天室人数
 socket.on("userList", (data) => {
   $(".list-right .title").text(`多人聊天室(${data.length})`);
+  $("#userList").html("");
+  data.forEach((item) => {
+    let li = `
+    <li>
+        <div class="img">
+            <img src="${item.avatar}" alt="" />
+        </div>
+        <p>${item.username}</p>
+    </li> `;
+    $("#userList").append(li);
+  });
+});
+
+// 监听用户离开的消息
+socket.on("delUser", (data) => {
+  $("#users").append(`
+    <div class="broadcast">${data.username}离开了群聊</div>
+    `);
+  scrollIntoView();
+});
+
+// 聊天功能
+$("#btnSend").on("click", function () {
+  let content = $("#msgSend").val().trim();
+  $("#msgSend").val("");
+  if (!content) {
+    alert("请输入内容");
+    return false;
+  }
+  socket.emit("sendMsg", {
+    msg: content,
+    username: username,
+    avatar: avatar,
+  });
+});
+// 监听聊天的消息
+socket.on("receiveMsg", (data) => {
+  // 判断是否是自己的消息
+  if (data.username === username) {
+    $("#users").append(`
+    <div class='message-content'>
+        <div class="my-user">
+              <div class="word">
+                ${data.msg}
+              </div>
+              <div class="img">
+                <img src="${data.avatar}" alt="" />
+              </div>
+            </div>
+            </div>
+        `);
+  } else {
+    $("#users").append(`
+    <div class='message-content'>
+        <div class="other-user">
+              <div class="img">
+                <img src="${data.avatar}" alt="" />
+              </div>
+              <div class="word">
+                ${data.username} | 
+                ${data.msg}
+              </div>
+            </div>
+            </div>
+        `);
+  }
+
+  scrollIntoView();
+});
+
+// 当前元素的底部滚动到可视区, 有兼容问题.
+function scrollIntoView() {
+  $("#users").children(":last").get(0).scrollIntoView(false);
+}
+// 发送图片
+$("#file").on("change", function () {
+  console.log(this.files[0]);
+  let file = this.files[0];
+  //H5的fileReader
+  let fr = new FileReader();
+  fr.readAsDataURL(file);
+  fr.onload = () => {
+    socket.emit("sendImage", {
+      username: username,
+      avatar: avatar,
+      img: fr.result,
+    });
+  };
+});
+//监听图片聊天信息
+socket.on("receiveImage", (data) => {
+  console.log(data);
+  // 判断是否是自己的消息
+  if (data.username === username) {
+    $("#users").append(`
+      <div class='message-content'>
+        <div class="my-user">
+              <div class="word">
+                <img src="${data.img}">
+              </div>
+              <div class="img">
+                <img src="${data.avatar}" alt="" />
+              </div>
+          </div>
+        </div>
+        `);
+  } else {
+    $("#users").append(`
+     <div class='message-content'>
+        <div class="other-user">
+              <div class="img">
+                <img src="${data.avatar}" alt="" />
+              </div>
+              <div class="word">
+                <span>${data.username}</span>
+                <img src="${data.img}">
+              </div>
+            </div>
+        </div>
+        `);
+  }
+
+  // 保证所有的图片加载完成后在滚动到底部
+  $("#users img:last").on("load", function () {
+    scrollIntoView();
+  });
 });
